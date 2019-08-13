3ca16d9f0afb2cc59074b0553f93f070aec4964a
luzifong <luzifengms@outlook.com>
2019-08-13 16:00:51 +0800

    test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("？？？!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
